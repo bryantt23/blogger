3602fa39b7521b02d43628b521305bf49552a5b1
bryant tee <bryantt23@gmail.com>
2014-12-22 01:46:32 -0800

    13 done

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,11 @@
 class TagsController < ApplicationController
-def show
-  @tag = Tag.find(params[:id])
-end
+  before_filter :require_login, only: [ :destroy ]
+  def index 
+    @tags = Tag.all
+  end
+  
+  def show
+    @tag = Tag.find(params[:id])
+  end
 
 end
