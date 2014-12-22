21f9364ec9f968b260475cbc5361800e1a72bbe1
bryant tee <bryantt23@gmail.com>
2014-12-21 22:34:02 -0800

    d

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ end
 def create
     @article = Article.new(article_params)
     @article.save
+  flash.notice = "Article '#{@article.title}' Created!"
 
     redirect_to article_path(@article)
 end
@@ -29,5 +30,21 @@ def destroy
     
     redirect_to articles_path
 end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  @article.update(article_params)
+
+  flash.notice = "Article '#{@article.title}' Updated!"
+
+  redirect_to article_path(@article)
+end
+
+
+
 
 end
